4e6ce86cd5980f4a199159866664932ad51e5ba6
Jeong-Yun-Kim <jeongyun061030@gmail.com>
2026-05-22 03:12:23 +0900

    .

--- build/mirobot_master_slave/colcon_command_prefix_setup_py.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/kjy/Mirobot_ros2/install/mirobot_description/share/mirobot_description/package.sh"
-. "/home/kjy/Mirobot_ros2/install/mirobot_driver/share/mirobot_driver/package.sh"
